06cc3d71551217d54066c8d923417bddd0833a34
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-02-17 21:55:00 +0900

    UIImageViewの生成に関する処理修正

--- UIPageControl/UIPageControl/ViewController.swift
+++ UIPageControl/UIPageControl/ViewController.swift
@@ -16,47 +16,70 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // scrollViewの画面表示サイズを指定
         scrollView = UIScrollView(frame: CGRect(x: 0, y: 200, width: self.view.frame.size.width, height: self.view.frame.size.height))
-        // scrollViewのサイズを指定（幅は1メニューに表示するViewの幅×メニュー数）
+        // scrollViewのサイズを指定（幅は1メニューに表示するViewの幅×ページ数）
         scrollView.contentSize = CGSize(width: self.view.frame.size.width*3, height: 200)
+        // scrollViewのデリゲートになる
         scrollView.delegate = self
         // メニュー単位のスクロールを可能にする
         scrollView.isPagingEnabled = true
         // 水平方向のスクロールインジケータを非表示にする
         scrollView.showsHorizontalScrollIndicator = false
-        
         self.view.addSubview(scrollView)
         
-        let view1 = UIView(frame: CGRect(x: self.view.frame.size.width, y: 0, width: self.view.frame.size.width, height: 200))
-        view1.backgroundColor = UIColor.cyan
-        scrollView.addSubview(view1)
+        // scrollView上にUIImageViewをページ分追加する(今回は3ページ分)
+        let imageView1 = createImageView(x: 0, y: 0, width: self.view.frame.size.width, height: 200, image: "image1")
+        scrollView.addSubview(imageView1)
         
-        let view2 = UIView(frame: CGRect(x: self.view.frame.size.width*2, y: 0, width: self.view.frame.size.width, height: 200))
-        view2.backgroundColor = UIColor.red
-        scrollView.addSubview(view2)
+        let imageView2 = createImageView(x: self.view.frame.size.width, y: 0, width: self.view.frame.size.width, height: 200, image: "image2")
+        scrollView.addSubview(imageView2)
         
-        let button = UIButton()
-        button.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 200)
-        button.backgroundColor = UIColor.green
-        let image1 = UIImage(named: "test1")
-        button.setImage(image1, for: .normal)
-        button.imageView?.contentMode = .scaleAspectFit
-        scrollView.addSubview(button)
+        let imageView3 = createImageView(x: self.view.frame.size.width*2, y: 0, width: self.view.frame.size.width, height: 200, image: "image3")
+        scrollView.addSubview(imageView3)
         
-        button.addTarget(self,
-                         action: #selector(ViewController.test(sender:)),
-                         for: .touchUpInside)
+        //        let button = UIButton()
+        //        button.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 200)
+        //        let image1 = UIImage(named: "image1")
+        //        button.setImage(image1, for: .normal)
+        //        button.imageView?.contentMode = .scaleAspectFit
+        //        scrollView.addSubview(button)
+        //
+        //        button.addTarget(self,
+        //                         action: #selector(ViewController.test(sender:)),
+        //                         for: .touchUpInside)
         
-        pageControl = UIPageControl(frame: CGRect(x: 0, y: 350, width: self.view.frame.size.width, height: 50))
+//        // ベタがきが版（消す）
+//        let imageView1 = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 200))
+//        let image1 = UIImage(named:  "image1")
+//        imageView1.image = image1
+//        scrollView.addSubview(imageView1)
+        
+        // pageControlの表示位置とサイズの設定
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: 370, width: self.view.frame.size.width, height: 30))
+        // pageControlのページ数を設定
         pageControl.numberOfPages = 3
+        // pageControlのドットの色
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        // pageControlの現在のページのドットの色
+        pageControl.currentPageIndicatorTintColor = UIColor.black
         self.view.addSubview(pageControl)
     }
     
-    @objc func test(sender : AnyObject) {
-        print("テスト")
+    //    @objc func test(sender : AnyObject) {
+    //        print("テスト")
+    //    }
+    
+    // UIImageViewを生成
+    func createImageView(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, image: String) -> UIImageView {
+        let imageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: height))
+        let image = UIImage(named:  image)
+        imageView.image = image
+        return imageView
     }
 }
 
+// scrollViewのページ移動に合わせてpageControlの表示も移動させる
 extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
